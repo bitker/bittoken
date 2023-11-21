eac8ec03689ffbbb6a28e31deb22c5c6931e43b6
bitker <my@bitke.net>
2023-11-22 00:14:00 +0800

    添加

---
+++ token/gf.go
@@ -0,0 +1,81 @@
+package token
+
+import (
+	"context"
+	"strings"
+
+	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/os/gctx"
+	"github.com/gogf/gf/v2/text/gstr"
+)
+
+// 初始化参数
+func NewGtoken(keys string) *token {
+	ctx := gctx.New()
+	return &token{
+		AuthExcludePaths: g.Cfg().MustGet(ctx, "BitToken.Exclude", "/login").Strings(),
+		CacheKey:         keys,
+		Timeout:          g.Cfg().MustGet(ctx, "BitToken.Timeout", "1000").Int(),
+		MaxRefresh:       g.Cfg().MustGet(ctx, "BitToken.Refresh", "1000").Int(),
+		EncryptKey:       g.Cfg().MustGet(ctx, "BitToken.EncryptKey").Bytes(),
+		MultiLogin:       g.Cfg().MustGet(ctx, "BitToken.MultiLogin").Bool(),
+	}
+}
+
+func (t *token) ParsToken(r *ghttp.Request) (res *cacheRes, err error) {
+	authHeader := r.Header.Get("Authorization")
+	token := ""
+	if authHeader != "" {
+		parts := strings.SplitN(authHeader, " ", 2)
+		if !(len(parts) == 2 && parts[0] == "Bearer") {
+			return nil, gerror.New("token 格式异常")
+		} else if parts[1] == "" {
+			return nil, gerror.New("token为空")
+		}
+
+		token = parts[1]
+	} else {
+		token = r.Get("token").String()
+		if token == "" {
+			return nil, gerror.New("token为空")
+		}
+	}
+	res, err = t.ValidToken(r.Context(), token)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (m *token) AuthPath(ctx context.Context, urlPath string) bool {
+	// 去除后斜杠
+	if strings.HasSuffix(urlPath, "/") {
+		urlPath = gstr.SubStr(urlPath, 0, len(urlPath)-1)
+	}
+
+	// 排除路径处理，到这里nextFlag为true
+	for _, excludePath := range m.AuthExcludePaths {
+		tmpPath := excludePath
+		// 前缀匹配
+		if strings.HasSuffix(tmpPath, "/*") {
+			tmpPath = gstr.SubStr(tmpPath, 0, len(tmpPath)-2)
+			if gstr.HasPrefix(urlPath, tmpPath) {
+				// 前缀匹配不拦截
+				return false
+			}
+		} else {
+			// 全路径匹配
+			if strings.HasSuffix(tmpPath, "/") {
+				tmpPath = gstr.SubStr(tmpPath, 0, len(tmpPath)-1)
+			}
+			if urlPath == tmpPath {
+				// 全路径匹配不拦截
+				return false
+			}
+		}
+	}
+
+	return true
+}
---
+++ token/token.go
@@ -0,0 +1,216 @@
+package token
+
+import (
+	"context"
+
+	"github.com/gogf/gf/v2/crypto/gaes"
+	"github.com/gogf/gf/v2/crypto/gmd5"
+	"github.com/gogf/gf/v2/encoding/gbase64"
+	"github.com/gogf/gf/v2/encoding/gjson"
+	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gtime"
+	"github.com/gogf/gf/v2/text/gstr"
+	"github.com/gogf/gf/v2/util/gconv"
+	"github.com/gogf/gf/v2/util/grand"
+)
+
+type token struct {
+	TokenDelimiter string
+
+	AuthExcludePaths []string //排除项
+	// 缓存key
+	CacheKey string
+	// 超时时间 默认10天（毫秒）
+	Timeout int
+	// 缓存刷新时间 默认为超时时间的一半（毫秒）
+	MaxRefresh int
+	// Token加密key
+	EncryptKey []byte
+	// 是否支持多端登录，默认false
+	MultiLogin bool
+}
+
+type tokenRes struct {
+	Uuid  string
+	Key   string
+	Token string
+}
+
+type cacheRes struct {
+	Key       string
+	Uuid      string
+	Data      interface{}
+	CreatedAt int64
+	RefreshAt int64
+}
+
+// 解密返回
+type decryptRes struct {
+	Key  string
+	Uuid string
+}
+
+// 生成TOKEN
+func (t *token) Generate(ctx context.Context, userKey string, data interface{}) (token string, err error) {
+
+	if t.MultiLogin {
+		// 支持多端重复登录，返回相同token 通过userkey获取数据然后生成相同TOKEN
+		res, err := t.getToken(ctx, userKey)
+		if err != nil {
+			return "", err
+		}
+		tokenRes, err := t.EncryptToken(ctx, userKey, res.Uuid)
+		if err != nil {
+			return "", err
+		}
+		return tokenRes.Token, nil
+	}
+	res, err := t.EncryptToken(ctx, userKey, "")
+
+	if err != nil {
+		return
+	}
+	//进行缓存
+	cacheKey := t.CacheKey + userKey
+	userCache := &cacheRes{
+		Key:       userKey,
+		Uuid:      res.Uuid,
+		Data:      data,
+		CreatedAt: gtime.Now().TimestampMilli(),
+		RefreshAt: gtime.Now().TimestampMilli() + gconv.Int64(t.MaxRefresh),
+	}
+
+	token = res.Token
+	err = t.setCache(ctx, cacheKey, userCache)
+
+	return
+}
+
+// 加密返回token结构体
+func (t *token) EncryptToken(ctx context.Context, userKey string, uuid string) (Res *tokenRes, err error) {
+	if userKey == "" {
+		return
+	}
+
+	if uuid == "" {
+		// 重新生成uuid
+		newUuid, err := gmd5.Encrypt(grand.Letters(10))
+		if err != nil {
+			return nil, err
+		}
+		uuid = newUuid
+	}
+
+	tokenStr := userKey + t.TokenDelimiter + uuid
+
+	token, err := gaes.Encrypt([]byte(tokenStr), t.EncryptKey)
+	if err != nil {
+		return
+	}
+
+	return &tokenRes{
+		Uuid:  uuid,
+		Key:   userKey,
+		Token: gbase64.EncodeToString(token),
+	}, nil
+}
+
+// DecryptToken token解密方法
+func (m *token) DecryptToken(ctx context.Context, token string) (res *decryptRes, err error) {
+	if token == "" {
+		return
+	}
+
+	token64, err := gbase64.Decode([]byte(token))
+	if err != nil {
+		return
+	}
+	decryptToken, err := gaes.Decrypt(token64, m.EncryptKey)
+	if err != nil {
+		return
+	}
+	tokenArray := gstr.Split(string(decryptToken), m.TokenDelimiter)
+	if len(tokenArray) < 2 {
+		err = gerror.New("token异常")
+		return
+	}
+	return &decryptRes{Key: tokenArray[0], Uuid: tokenArray[1]}, nil
+}
+
+// getToken 通过userKey获取Token
+func (t *token) getToken(ctx context.Context, userKey string) (res *cacheRes, err error) {
+	cacheKey := t.CacheKey + userKey
+
+	userCacheResp, err := t.getCache(ctx, cacheKey)
+	if err != nil {
+		return
+	}
+
+	nowTime := gtime.Now().TimestampMilli()
+	refreshTime := userCacheResp.RefreshAt
+
+	// 需要进行缓存超时时间刷新
+	if gconv.Int64(refreshTime) == 0 || nowTime > gconv.Int64(refreshTime) {
+		userCacheResp.CreatedAt = gtime.Now().TimestampMilli()
+		userCacheResp.RefreshAt = gtime.Now().TimestampMilli() + gconv.Int64(t.MaxRefresh)
+		err = t.setCache(ctx, cacheKey, userCacheResp)
+		if err != nil {
+			return
+		}
+	}
+
+	return userCacheResp, nil
+}
+
+func (t *token) setCache(ctx context.Context, cacheKey string, userCache *cacheRes) error {
+	cacheValueJson, err := gjson.Encode(userCache)
+	if err != nil {
+		return err
+	}
+	_, err = g.Redis().Do(ctx, "SETEX", cacheKey, t.Timeout/1000, cacheValueJson)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (t *token) getCache(ctx context.Context, cacheKey string) (res *cacheRes, err error) {
+	userCacheJson, err := g.Redis().Do(ctx, "GET", cacheKey)
+	if err != nil {
+		return
+	}
+	if userCacheJson.IsNil() {
+		err = gerror.New("login timeout or not login")
+		return
+	}
+	err = gjson.DecodeTo(userCacheJson, &res)
+	return
+}
+
+// validToken 验证Token
+func (t *token) ValidToken(ctx context.Context, token string) (res *cacheRes, err error) {
+	if token == "" {
+		err = gerror.New("token is empty")
+		return
+	}
+
+	decryptToken, err := t.DecryptToken(ctx, token)
+	if err != nil {
+		return
+	}
+
+	userKey := decryptToken.Key
+	uuid := decryptToken.Uuid
+
+	userCacheResp, err := t.getToken(ctx, userKey)
+	if err != nil {
+		return
+	}
+
+	if uuid != userCacheResp.Uuid {
+		err = gerror.New("token 异常")
+		return
+	}
+
+	return userCacheResp, nil
+}
